a79f36be286119dad34108ef7db4e8506fb58b0c
agirondo <agirondo@protonmail.com>
2022-05-18 21:41:20 +0200

    ARMA

--- src/src.py
+++ src/src.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import time
 import requests
-from statsmodels.tsa.ar_model import AutoReg as AR
+from statsmodels.tsa.arima.model import ARIMA
 
 def get_data(i,countries): # retrieves dictionary with country as key and dataframe as value
     data={}
@@ -17,9 +17,9 @@ def get_data(i,countries): # retrieves dictionary with country as key and datafr
             pass
     return data
 
-def get_df(data):
+def get_df(data,p=1,d=0,q=1):
     countries=data.keys()
-    resultado=pd.DataFrame(columns=['country','error2019','error2020'])
+    resultado=pd.DataFrame(columns=['country','error2015_2019','error2020'])
     for z in countries:
         
         df=data[z].iloc[:2020]
@@ -27,16 +27,23 @@ def get_df(data):
         if df.isnull().values.any():
             continue
         else:
-            train, test = df.value[:-2], df.value[-2:-1]
-            modelo=AR(train, lags=5).fit()
+            '''train, test = df.value[:-2], df.value[-2:-1]
+            modelo=ARIMA(train,order=(p,d,q)).fit()
             pred=modelo.predict(len(train)-1,len(train)-1)
-            error2019=(test[0]-pred[0])/pred[0]
+            error2019=test[0]-pred[0]'''
+
+            train, test = df.value[:-6], df.value[-6:-1]
+            modelo=ARIMA(train, order=(p,d,q)).fit()
+            pred=modelo.predict(len(train)-5,len(train)-1)
+            pred.index=['2015','2016','2017','2018','2019']
+            error2015_2019=(pred-test).abs().sum()/len(pred) 
 
             train, test = df.value[:-1], df.value[-1:]
-            modelo=AR(train, lags=5).fit()
+            modelo=ARIMA(train, order=(p,d,q)).fit()
             pred=modelo.predict(len(train)-1,len(train)-1)
-            error2020=(test[0]-pred[0])/pred[0]
+            error2020=test[0]-pred[0]
 
-            resultado=resultado.append({'country':z,'error2019':error2019,'error2020':error2020},ignore_index=True)
+
+            resultado=resultado.append({'country':z,'error2015_2019':error2015_2019,'error2020':error2020},ignore_index=True)
 
     return resultado
